1ed6e18f637c0169b694495418072a825f43f6ef
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-11-07 20:16:09 -0500

    Updating with sockets from server file every second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      timestamp: 'Nothing set yet.'
+    }
+
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <p className="App-intro">
+            This is the timer value: {this.state.timestamp}
+          </p>
+        </div>
+    );
+  }
 }
 
-export default App;
